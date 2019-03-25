268139ee6d57f58283fb541ed470aad5474727be
Léo <leo.lemieresoda@gmail.com>
2019-03-25 16:53:12 +0100

    test

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "graph.h"
+#include "tas.h"
+#include "pFile.h"
+
+enum eType {sain, immunisé, malade, mort};
+
+double lambda; //probabilité de tomber malade
+double beta; //probabilité de mourir
+double gamma; //probabilité d'être immunisé
 
 int main() {
     printf("Hello, World!\n");
