4c6f36d440a93e62190d44f9ddb258c0ba0b195b
Léo <leo.lemieresoda@gmail.com>
2019-04-04 21:38:10 +0200

    test

--- main.c
+++ main.c
@@ -8,52 +8,65 @@
 
 enum etat {sain, immunise, malade, mort}; //états possibles des individus
 
-typedef struct Individu{ //création d'une pour chaque individu
+typedef struct Individu{ //création d'une structure pour chaque individu
     enum etat statut;
     int date_debut_maladie;
 } individu;
 
 void initialisation(individu Matrice_adjacence);
-int maladie();
+void etat_futur_malade();
+void etat_futur_sain();
 
 double lambda; //probabilité de tomber malade
 double beta; //probabilité de mourir
 double gama; //probabilité d'être immunisé
-<<<<<<< HEAD
+
 int population;
-=======
+
 int population = 20 * 20;
 int population_longueur = 20;
->>>>>>> origin/master
+
 
 //Initialisation Matrice d'adjacence
 void initialisation(individu Matrice_adjacence) {
     ;
 }
 
-int maladie() {
+void etat_futur_malade() {
     int a;
     individu i;
     double aleatoire;
     aleatoire=(double)(rand()%100)/100;
     if (aleatoire<=1-gama) {
         a=0;
+        i.statut = immunise //individu immunisé
     }
     aleatoire=(double)(rand()%100)/100;
     if (a=!0 && aleatoire<=1-beta) {
-        a=1;
+        i.statut = mort; //individu mort
     }
     else {
-        a=2;
+        i.statut = malade; //individu malade
     }
-    return a;
+}
+
+void etat_futur_sain() {
+    individu i;
+    double aleatoire;
+    aleatoire=(double)(rand()%100)/100;
+    if (aleatoire<=1-lambda) {
+        i.statut = malade //individu malade
+    }
+    else {
+        i.statut = sain; //individu sain
+    }
 }
 
 int main() {
     srand(time(NULL)); // permet d'avoir tout le temps des nombres aléatoires
-<<<<<<< HEAD
+
     //initialisation();
-=======
+
     individu Matrice_adjacence[population][population];
     initialisation(Matrice_adjacence);
     float p = (float)(rand()%100)/100;
@@ -65,7 +78,8 @@ int main() {
             if(Matrice_adjacence[i][k].statut == )
         }
     }
->>>>>>> origin/master
-    maladie();
+
+    etat_futur_malade();
+    etat_futur_sain()
     return 0;
 }
